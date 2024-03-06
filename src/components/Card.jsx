94a052af4caf3f32227f74084ecaaaea35276582
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-07 01:21:06 +0530

    Added multiple cards with different data and learned abouts fragments...

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,28 @@
 // import React from 'react'
 
 function Card() {
+
+    const data = [
+        {image: "https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?q=80&w=1796&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", name: "Amazon", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta, aspernatur quae atque quaerat deserunt officia dignissimos possimus porro distinctio dolor?"},
+        {image: "https://images.unsplash.com/photo-1654573817889-296cad084c97?q=80&w=2062&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", name: "Flipkart", description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta, aspernatur quae atque quaerat deserunt."},
+        {image: "https://images.unsplash.com/photo-1563203369-26f2e4a5ccf7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", name: "Apple", description: "Lorem ipsum dolor sit amet consectetur."}
+    ];
+
   return (
-    <div className="w-full h-screen bg-zinc-200" >
-        <div className=" w-52 bg-zinc-100 absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] rounded-md overflow-hidden">
-            <div className="w-full h-32 bg-zinc-300">
-                <img className="w-full h-full object-cover" src="https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?q=80&w=1796&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-            </div>
-            <div className="w-full px-3 py-4" >
-                <h2 className="font-semibold">Amazon Basics.</h2>
-                <p className="text-xs mt-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta, aspernatur quae atque quaerat deserunt officia dignissimos possimus porro distinctio dolor?</p>
-            </div>
-        </div>
+    <div className="w-full h-screen flex items-center justify-center gap-10 bg-zinc-200" >
+        {
+            data.map((item, index) => (
+                <div key={index} className=" w-52 bg-zinc-100 rounded-md overflow-hidden">
+                    <div className="w-full h-32 bg-zinc-300">
+                        <img className="w-full h-full object-cover" src={item.image} alt="" />
+                    </div>
+                    <div className="w-full px-3 py-4" >
+                        <h2 className="font-semibold">{item.name}</h2>
+                        <p className="text-xs mt-4">{item.description}</p>
+                    </div>
+                </div>
+            ))
+        }
     </div>
   )
 }
