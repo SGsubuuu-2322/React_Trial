6e6ac0b7f8e72117a0025aa81f9af0a3cd03412d
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-07 11:06:10 +0530

    Added and learned the concepts for useState and BTS of useState()...

--- src/components/UseState.jsx
+++ src/components/UseState.jsx
@@ -1,15 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function UseState() {
-    const [val, setVal] = useState({name: "Subham", isBanned: false});
+  const [val, setVal] = useState({ name: "Subham", isBanned: false });
   return (
     <div>
-        <h1>Name: {val.name}</h1>
-        <h2>IsBanned: {val.isBanned.toString()}</h2>
+      <h1>Name: {val.name}</h1>
+      <h2>IsBanned: {val.isBanned.toString()}</h2>
 
-        <button onClick={()=> setVal({...val, isBanned: !val.isBanned})} className='px-3 py-2 mt-2 bg-blue-400 rounded-full'>Change</button>
+      <button
+        onClick={() => setVal({ ...val, isBanned: !val.isBanned })}
+        className={`px-3 py-2 mt-2 ${
+          val.isBanned ? "bg-blue-400" : "bg-red-400"
+        } rounded-full`}
+      >
+        Change
+      </button>
     </div>
-  )
+  );
 }
 
-export default UseState
+export default UseState;
