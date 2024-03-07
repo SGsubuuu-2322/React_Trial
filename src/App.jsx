96a25ccc71e84ccc4cad4317171092f28ed4c8f5
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-07 10:28:37 +0530

    Added and learned concepts and codes for state management using useState hooks...

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 // import React from "react";
 // import Card from "./components/Card";
-import Button from "./components/Button";
+// import Button from "./components/Button";
+import UseState from "./components/UseState";
 
 function App(){
   return(
    <>
     {/* <Card/> */}
-    <Button/>
+    {/* <Button/> */}
+    <UseState/>
    </>
   )
 }
---
+++ src/components/UseState.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+function UseState() {
+    const [val, setVal] = useState({name: "Subham", isBanned: false});
+  return (
+    <div>
+        <h1>Name: {val.name}</h1>
+        <h2>IsBanned: {val.isBanned.toString()}</h2>
+
+        <button onClick={()=> setVal({...val, isBanned: !val.isBanned})} className='px-3 py-2 mt-2 bg-blue-400 rounded-full'>Change</button>
+    </div>
+  )
+}
+
+export default UseState
