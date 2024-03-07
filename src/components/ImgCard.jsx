8e3c0662655dd3761ae924d9c70a9f104ad2ef32
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2024-03-07 17:51:03 +0530

    Debugged some codes..

--- src/components/ImgCard.jsx
+++ src/components/ImgCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { FaArrowAltCircleRight } from "react-icons/fa";
 
 function ImgCard() {
@@ -10,14 +10,14 @@ function ImgCard() {
         <img
           src="https://images.unsplash.com/photo-1707343848552-893e05dba6ac?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="image"
-          className={`"h-full w-full shrink-0 ${
+          className={`"h-full w-full shrink-0 transition-transform duration-300 ${
             val === false ? "-translate-x-[0%]" : "-translate-x-[100%]"
           } object-cover"`}
         />
         <img
           src="https://images.unsplash.com/photo-1607131168055-9ff89d69922f?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt="image"
-          className={`"h-full w-full shrink-0 ${
+          className={`"h-full w-full shrink-0 transition-transform duration-300 ${
             val === false ? "-translate-x-[0%]" : "-translate-x-[100%]"
           } object-cover"`}
         />
